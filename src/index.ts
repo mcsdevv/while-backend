9b0c3d0f7e614325e47f40e653af4b632c60254b
mcsdevv <mail@mcs.dev>
2021-04-03 21:43:59 +0100

    minor

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import bodyParser from "body-parser";
 import express from "express";
 
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 const app = express();
 const port = process.env.PORT || 3333;
 
@@ -9,7 +12,10 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  res.json({ Hello: "World" });
+  const user = await prisma.user.findUnique({
+    where: { id: 1 },
+  });
+  res.json({ Hello: user });
 });
 
 app.listen(port, () => {
