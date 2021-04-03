6729ac36899dd42ac4ba39e0187cb96a8a10bd33
mcsdevv <mail@mcs.dev>
2021-04-03 21:52:07 +0100

    minor

--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,20 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 const app = express();
-const port = process.env.PORT || 3333;
 
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
-  console.time("db");
+  // console.time("db");
   const user = await prisma.user.findUnique({
     where: { id: 1 },
   });
-  console.timeEnd("db");
+  // console.timeEnd("db");
   res.json({ Hello: user });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3001, () => {
+  console.log(`Example app listening at http://localhost:3001`);
 });
