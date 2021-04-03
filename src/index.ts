5ed6a9524c19ae7fc357cfd63461dcba3ac795cd
mcsdevv <mail@mcs.dev>
2021-04-03 21:45:41 +0100

    minor

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,11 @@ app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
 app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
+  console.time("db");
   const user = await prisma.user.findUnique({
     where: { id: 1 },
   });
+  console.timeEnd("db");
   res.json({ Hello: user });
 });
 
