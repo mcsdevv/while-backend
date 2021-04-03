e3c2809e4449a95a1c9f64d01c0946ec09939989
mcsdevv <mail@mcs.dev>
2021-04-03 22:41:26 +0100

    minor

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,9 @@ app.use(bodyParser.text({ type: "text/html" }));
 
 app.get("/", async (req, res) => {
   // console.time("db");
-  // const user = await prisma.user.findUnique({
-  //   where: { id: 1 },
-  // });
+  const user = await prisma.user.findMany();
   // console.timeEnd("db");
-  res.json({ Hello: process.env.DATABASE_URL });
+  res.json({ Hello: user });
 });
 
 app.listen(port, () => {
