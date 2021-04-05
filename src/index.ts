21e4b1173d84677956064fc3b9f749c2adad90b4
mcsdevv <mail@mcs.dev>
2021-04-06 00:20:46 +0100

    update callback url

--- src/index.ts
+++ src/index.ts
@@ -30,7 +30,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.GOOGLE_AUTH_CALLBACK,
     },
     async (accessToken: any, refreshToken: any, profile: any, done: any) => {
       try {
