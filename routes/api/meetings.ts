19074ce3b26905f3c89dfb3210fa2cb4c7b64a31
mcsdevv <mail@mcs.dev>
2021-05-23 23:26:58 +0100

    minor

--- routes/api/meetings.ts
+++ routes/api/meetings.ts
@@ -1,6 +1,6 @@
 // * Libraries
 import express from "express";
-import { PrismaClient } from "@prisma/client";
+import { Meeting, PrismaClient } from "@prisma/client";
 
 // * Middleware
 import { isAuthenticated } from "../../middleware/isAuthenticated";
@@ -12,7 +12,7 @@ const router = express.Router();
 router.get("/", isAuthenticated, async (req: any, res: express.Response) => {
   try {
     const { id } = req.user;
-    const meetings = await prisma.meeting.findMany({
+    const meetings: Meeting[] = await prisma.meeting.findMany({
       where: { creatorId: id },
     });
     res.status(200).json(meetings);
@@ -21,5 +21,20 @@ router.get("/", isAuthenticated, async (req: any, res: express.Response) => {
     res.status(500).send(err);
   }
 });
+
+router.post(
+  "/create",
+  isAuthenticated,
+  async (req: any, res: express.Response) => {
+    try {
+      const { id } = req.user;
+      const meeting = await prisma.meeting.create({ data: { creatorId: id } });
+      res.status(200).json(meeting);
+    } catch (err) {
+      console.log("err", err);
+      res.status(500).send(err);
+    }
+  }
+);
 
 module.exports = router;
