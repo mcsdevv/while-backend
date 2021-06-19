975d2b4f9016109a9bf8df840e8233546ab8f9c9
mcsdevv <mail@mcs.dev>
2021-06-19 18:38:48 +0100

    minor

--- routes/api/meetings.ts
+++ routes/api/meetings.ts
@@ -12,10 +12,32 @@ const router = express.Router();
 router.get("/", isAuthenticated, async (req: any, res: express.Response) => {
   try {
     const { id } = req.user;
+
     const meetings: Meeting[] = await prisma.meeting.findMany({
       where: { creatorId: id },
     });
+
     res.status(200).json(meetings);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).send(err);
+  }
+});
+
+router.get("/:id", isAuthenticated, async (req: any, res: express.Response) => {
+  try {
+    const { id: meetingId } = req.params;
+    const { id: userId } = req.user;
+
+    const meeting: Meeting | null = await prisma.meeting.findUnique({
+      where: { id: meetingId },
+    });
+
+    if (meeting && meeting.creatorId !== userId) {
+      res.status(403).send("Not authorized to access this resource.");
+    }
+
+    res.status(200).json(meeting);
   } catch (err) {
     console.log("err", err);
     res.status(500).send(err);
@@ -28,7 +50,9 @@ router.post(
   async (req: any, res: express.Response) => {
     try {
       const { id } = req.user;
+
       const meeting = await prisma.meeting.create({ data: { creatorId: id } });
+
       res.status(200).json(meeting);
     } catch (err) {
       console.log("err", err);
